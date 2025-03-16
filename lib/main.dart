21ad730a04009835da3dfc4780a4a85ebbf66334
Ayush Singh <ayushsingh30082005@gmail.com>
2025-03-16 10:37:21 +0530

    Added 'Black Screen'

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,47 @@ void main() {
 }
 
 class TypingApp extends StatelessWidget {
-  const TypingApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Text(
-            'Prototype 1: Black Screen',
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
+        backgroundColor: Color(0xFF1F1F1F),
+        body: Stack(
+          children: [
+            Positioned(
+              left: 50,
+              top: 100,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Word 1'),
+              ),
+            ),
+            Positioned(
+              right: 50,
+              top: 100,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Word 2'),
+              ),
+            ),
+            Positioned(
+              left: 50,
+              bottom: 100,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Word 3'),
+              ),
+            ),
+            Positioned(
+              right: 50,
+              bottom: 100,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Word 4'),
+              ),
+            ),
+          ],
         ),
       ),
     );
