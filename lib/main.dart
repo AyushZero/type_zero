17aacd8452f43d2abcd2c224f03846a169ce63de
Ayush Singh <ayushsingh30082005@gmail.com>
2025-03-16 10:42:31 +0530

    Added Longer Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -19,79 +19,33 @@ class TypingApp extends StatelessWidget {
         backgroundColor: Color(0xFF1F1F1F),
         body: Stack(
           children: [
-            Positioned(
-              left: 100,
-              top: 50,
-              child: SizedBox(
-                width: 120,
-                height: 60,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey[800],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: Text('Word 1', style: TextStyle(color: Colors.white)),
-                ),
-              ),
-            ),
-            Positioned(
-              left: 300,
-              top: 20,
-              child: SizedBox(
-                width: 120,
-                height: 60,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey[800],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: Text('Word 2', style: TextStyle(color: Colors.white)),
-                ),
-              ),
-            ),
-            Positioned(
-              right: 300,
-              top: 20,
-              child: SizedBox(
-                width: 120,
-                height: 60,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey[800],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: Text('Word 3', style: TextStyle(color: Colors.white)),
-                ),
-              ),
-            ),
-            Positioned(
-              right: 100,
-              top: 50,
-              child: SizedBox(
-                width: 120,
-                height: 60,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey[800],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: Text('Word 4', style: TextStyle(color: Colors.white)),
-                ),
-              ),
-            ),
+            _buildButton(left: 100, top: 50, text: 'Word 1'),
+            _buildButton(left: 300, top: 20, text: 'Word 2'),
+            _buildButton(right: 300, top: 20, text: 'Word 3'),
+            _buildButton(right: 100, top: 50, text: 'Word 4'),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButton({double? left, double? right, required double top, required String text}) {
+    return Positioned(
+      left: left,
+      right: right,
+      top: top,
+      child: SizedBox(
+        width: 120,
+        height: 220,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.grey[800],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          child: Text(text, style: TextStyle(color: Colors.white)),
         ),
       ),
     );
