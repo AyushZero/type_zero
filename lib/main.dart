618f511bb0f0f9cc0876a1981e8d5b57e72841ce
Ayush Singh <ayushsingh30082005@gmail.com>
2025-03-16 10:43:54 +0530

    Added Typing

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,55 @@ void main() {
   runApp(TypingApp());
 }
 
-class TypingApp extends StatelessWidget {
+class TypingApp extends StatefulWidget {
+  @override
+  _TypingAppState createState() => _TypingAppState();
+}
+
+class _TypingAppState extends State<TypingApp> {
+  String typedText = "";
+  List<String> sentences = [
+    "Hello world",
+    "Flutter is amazing",
+    "Typing speed test",
+    "Welcome to adaptive typing",
+    "Let's build something cool",
+  ];
+
+  void addWord(String word) {
+    setState(() {
+      typedText += (typedText.isEmpty ? "" : " ") + word;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color(0xFF1F1F1F),
-        body: Stack(
+        body: Column(
           children: [
-            _buildButton(left: 100, top: 50, text: 'Word 1'),
-            _buildButton(left: 300, top: 20, text: 'Word 2'),
-            _buildButton(right: 300, top: 20, text: 'Word 3'),
-            _buildButton(right: 100, top: 50, text: 'Word 4'),
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: Text(
+                  typedText,
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Stack(
+                children: [
+                  _buildButton(left: 100, top: 50, text: sentences[0]),
+                  _buildButton(left: 300, top: 20, text: sentences[1]),
+                  _buildButton(right: 300, top: 20, text: sentences[2]),
+                  _buildButton(right: 100, top: 50, text: sentences[3]),
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -38,14 +74,14 @@ class TypingApp extends StatelessWidget {
         width: 120,
         height: 220,
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () => addWord(text),
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.grey[800],
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
             ),
           ),
-          child: Text(text, style: TextStyle(color: Colors.white)),
+          child: Text(text, style: TextStyle(color: Colors.white), textAlign: TextAlign.center),
         ),
       ),
     );
